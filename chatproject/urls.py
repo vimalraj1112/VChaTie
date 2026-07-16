12947b199d110849bcf53ca3e4a65dbe4f309931
vimalraj1112 <vimalraj07145@gmail.com>
2026-07-17 01:11:37 +0530

    VChaTie

--- chatproject/urls.py
+++ chatproject/urls.py
@@ -18,10 +18,14 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
+
 urlpatterns = [
+    path('', RedirectView.as_view(url='/chatie/')),
     path('admin/', admin.site.urls),
-    path('',include('chatie.urls')),
-    path('api/',include('chatie.api_urls'))
+    path('chatie/', include('chatie.urls')),
+    path('api/', include('chatie.api_urls')),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
